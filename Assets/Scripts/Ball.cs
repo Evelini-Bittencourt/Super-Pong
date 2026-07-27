9b5ecc1d17191ec41c86da01050175903e53b36f
Evelini Bittencourt Riemer <bittencourtriemerevelini@gmail.com>
2026-07-27 19:13:57 -0300

    feat: implementa a colisão da bola com os paddles

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,11 +5,15 @@ public class Ball : MonoBehaviour
 {
    public float speed=5;
    private Vector2 direction = Vector2.one; //o mesmo que: private Vector2 direction = new Vector2(1,1);
+   //forma de fazer referência a outros elementos da cena. Defini-se uma variável com o tipo Transform (componente que guarda as as localizaçãoes dos objetos) e depois arrasta o objeto até o epaço que vai aparecer na interface da unity, para fazer a ligação entre os objetos 
+   public Transform paddleLeft;
+   public Transform paddleRight;
 
    private void Update()
     {
       Move();
       BounceTopAndBottom();  
+      BounceWithPaddles();
     }
     private void Move()
     {
@@ -31,5 +35,33 @@ public class Ball : MonoBehaviour
             direction.y=1;
         }
 
+    }
+    private void BounceWithPaddles()
+    {
+        float paddleWidth= 0.5f;
+        float paddleHeight= 2f;
+        float ballSize= 0.5f;
+
+        if (direction.x > 0)//a bola está se movendo para a direita 
+        {
+            if((transform.position.x + ballSize/2f) > (paddleRight.position.x-paddleWidth/2f)
+            && (transform.position.x + ballSize/2f) < (paddleRight.position.x + paddleWidth/2f)
+            && transform.position.y > (paddleRight.position.y - paddleHeight/2f)
+            && transform.position.y < (paddleRight.position.y + paddleHeight/2f)
+            )
+            {
+                direction.x=-1;
+            }
+        }else if (direction.x < 0)
+        {
+            if((transform.position.x - ballSize/2f) > (paddleLeft.position.x - paddleWidth/2f)
+            && (transform.position.x - ballSize/2f) < (paddleLeft.position.x + paddleWidth/2f)
+            && transform.position.y > (paddleLeft.position.y - paddleHeight/2f)
+            && transform.position.y < (paddleLeft.position.y + paddleHeight/2f)
+            )
+            {
+                direction.x=1;
+            }
+        }
     }
 }
